491edc9c347d208cbeef9bd1db6f04bfd6278ed5
Brenda Bonareri <brendabjb24@gmail.com>
2025-08-10 17:37:39 +0300

    fix: properly register CustomUser with admin to display is_staff and is_employer fields

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff', 'is_employer', 'is_active')
+    list_filter = ('is_staff', 'is_employer', 'is_active')
 
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_employer', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_employer', 'is_active'),
+        }),
+    )
 
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
+try:
+    admin.site.unregister(CustomUser)
+except admin.sites.NotRegistered:
+    pass
 
+admin.site.register(CustomUser, CustomUserAdmin)
